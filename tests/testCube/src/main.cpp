fd4106cc31249493f7f23f36cb1b4fdd77dc2152
MorcilloSanz <amorcillosanz@gmail.com>
2023-06-10 16:31:54 +0200

    Tests

---
+++ tests/testCube/src/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+
+#include <engine/renderer/Renderer.h>
+#include <engine/renderer/TrackballCamera.h>
+
+#include <GLFW/glfw3.h>
+
+const int WIDTH = 1280;
+const int HEIGHT = 900;
+GLFWwindow* window;
+
+Renderer::Ptr renderer;
+
+// x y z  r g b
+std::vector<Vec3f> cubeVertices {
+    Vec3f(-0.5, -0.5,  0.5,   0.0f, 0.0f, 1.0f),
+    Vec3f( 0.5, -0.5,  0.5,   1.0f, 0.0f, 1.0f),
+    Vec3f( 0.5,  0.5,  0.5,   0.0f, 1.0f, 1.0f),
+    Vec3f(-0.5,  0.5,  0.5,   0.0f, 1.0f, 0.5f),
+    Vec3f(-0.5, -0.5, -0.5,   0.0f, 0.0f, 1.0f),
+    Vec3f( 0.5, -0.5, -0.5,   1.0f, 0.0f, 1.0f),
+    Vec3f( 0.5,  0.5, -0.5,   0.0f, 1.0f, 1.0f),
+    Vec3f(-0.5,  0.5, -0.5,   0.0f, 1.0f, 0.5f)
+};
+
+std::vector<unsigned int> cubeIndices {
+    0, 1, 2,  1, 5, 6,  7, 6, 5,
+    2, 3, 0,  6, 2, 1,  5, 4, 7,
+    4, 0, 3,  4, 5, 1,  3, 2, 6,
+    3, 7, 4,  1, 0, 4,  6, 7, 3 
+};
+
+int main() {
+
+    // Create window
+    if (!glfwInit()) {
+        std::cout << "Couldn't initialize window" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Perspective cube example", NULL, NULL);
+    
+    if (!window) glfwTerminate();
+
+    glfwMakeContextCurrent(window);
+
+    // Renderer
+    renderer = Renderer::New(WIDTH, HEIGHT);
+
+    double aspectRatio = static_cast<double>(WIDTH) / HEIGHT;
+    TrackballCamera camera = TrackballCamera::perspectiveCamera(glm::radians(45.0f), aspectRatio, 0.1, 1000);
+    camera.zoom(-2.5);
+
+    renderer->setCamera(camera);
+
+    // Scene
+    Polytope::Ptr polytopeCube = Polytope::New(cubeVertices, cubeIndices);
+
+    Group::Ptr group = Group::New();
+    group->add(polytopeCube);
+
+    Scene::Ptr scene = Scene::New();
+    scene->addGroup(group);
+
+    renderer->addScene(scene);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+
+        // Update scene
+        polytopeCube->rotate(0.55, glm::vec3(1, 0, 1));
+
+        // Draw scene
+        renderer->clear();
+        renderer->render();
+        
+        // Update window
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Destroy window
+    glfwTerminate();
+
+    return 0;
+}
---
+++ tests/testImGuiGLFW/README.md
@@ -0,0 +1,4 @@
+## Dependencies
+
+* [GLFW](https://github.com/glfw/glfw) for creating a window with an OpenGL context
+* [IMGUI](https://github.com/ocornut/imgui) for the user interface

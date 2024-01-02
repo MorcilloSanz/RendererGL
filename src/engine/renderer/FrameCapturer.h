a03837d53da14c897d10fd10a65cddb1de92da6b
MorcilloSanz <amorcillosanz@gmail.com>
2024-01-02 13:49:10 +0100

    MSAA samples

--- src/engine/renderer/FrameCapturer.h
+++ src/engine/renderer/FrameCapturer.h
@@ -14,6 +14,7 @@ class FrameCapturer {
     GENERATE_PTR(FrameCapturer)
 private:
     unsigned int width, height;
+    unsigned int samples;
 
     FrameBuffer::Ptr frameBuffer;
     FrameBuffer::Ptr intermediateFrameBuffer;
@@ -25,9 +26,9 @@ private:
 
     glm::vec3 backgroundColor;
 public:
-    FrameCapturer(unsigned int _width, unsigned int _height) 
-        : width(_width), height(_height), backgroundColor(0.1f, 0.1f, 0.1f) {
-        updateViewPort(width, height);
+    FrameCapturer(unsigned int _width, unsigned int _height, unsigned int _samples = 4) 
+        : width(_width), height(_height), samples(_samples), backgroundColor(0.1f, 0.1f, 0.1f) {
+        updateViewPort(width, height, samples);
     }
 
     FrameCapturer()
@@ -36,16 +37,17 @@ public:
 
     ~FrameCapturer() = default;
 public:
-    void updateViewPort(unsigned int width, unsigned int height) {
+    void updateViewPort(unsigned int width, unsigned int height, unsigned int samples = 4) {
 
         this->width = width;
         this->height = height;
+        this->samples = samples;
 
         // Configure MSAA framebuffer
         frameBuffer = FrameBuffer::New();
 
         // Create a multisampled color attachment texture
-        textureColorBufferMultiSampled = MultiSampleTexture::New(width, height);
+        textureColorBufferMultiSampled = MultiSampleTexture::New(width, height, samples);
         frameBuffer->toTexture(GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, textureColorBufferMultiSampled->getID());
 
         // Create a (also multisampled) renderbuffer object for depth and stencil attachments
@@ -70,12 +72,12 @@ public:
     }
 
     void startCapturing() {
-        // Blending
+
         glDisable(GL_DEPTH_TEST);
         glEnable(GL_BLEND);
         glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
         glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-        // Clear frame buffer
+
         frameBuffer->bind();
         glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -83,10 +85,10 @@ public:
     }
 
     void finishCapturing() {
-         // 2. now blit multisampled buffer(s) to normal colorbuffer of intermediate FBO. Image is stored in screenTexture
+
         intermediateFrameBuffer->blitFrom(frameBuffer, width, height);
-        // 3. now render quad with scene's visuals as its texture image
         intermediateFrameBuffer->unbind();
+
         glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glDisable(GL_DEPTH_TEST);
